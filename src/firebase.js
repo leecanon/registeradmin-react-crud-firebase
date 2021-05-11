3fc96203f9e87aae64899189c25eaff61c5bb4b1
leecanon <victor.chuquin@upeu.edu.pe>
2021-05-11 09:00:10 -0500

    crudwithfirebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDyHofH1_aHRFsRP6B8df2bImQkqe5sCOA",
+    authDomain: "gobalriego.firebaseapp.com",
+    databaseURL: "https://gobalriego-default-rtdb.firebaseio.com",
+    projectId: "gobalriego",
+    storageBucket: "gobalriego.appspot.com",
+    messagingSenderId: "133902448418",
+    appId: "1:133902448418:web:83c4e588201db5e66f577b"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
